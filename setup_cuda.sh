131311d45af16405c0926f03aea235fc2a262aac
alei7 <rale@1c.ru>
2017-10-19 16:57:53 +0300

    fixes

--- setup_cuda.sh
+++ setup_cuda.sh
@@ -1,1 +1,9 @@
 yum install kernel-devel kernel-headers -y
+7za e driver/NVIDIA-Linux-x86_64-384.90.zip
+if [! -e 1.ok]
+	echo 'blacklist nouveau' >> /etc/modprobe.d/blacklist.conf
+	dracut /boot/initramfs-$(uname -r).img $(uname -r) --force
+	touch 1.ok
+	reboot
+fi
+bash driver/NVIDIA-Linux-x86_64-384.90.run
